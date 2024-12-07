65b886caae1fb991e80a6a55950a7e6692b4f957
Lívia Leal <liviabenine@gmail.com>
2024-12-07 20:04:36 -0300

    paginação incial

--- crud_resenha_filme/src/pages/home.tsx
+++ crud_resenha_filme/src/pages/home.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Grid from "../ui/components/grid";
+import Footer from "../ui/components/pagination";
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,60 +15,59 @@ const Home = () => {
   };
 
   return (
-    <div className="g-24 component-div">
-      <div className="g-24 align-center">
-        <h1>Resenha de Filmes</h1>
-        <button onClick={openModal} className="rounded-3 shadow-lighter">
-          Adicionar resenha
-        </button>
-        <input
-          type="search"
-          name=""
-          id=""
-          placeholder="Procure um filme"
-          className="p-10 rounded-3 shadow-lighter"
-          value={searchItem}
-          onChange={(e)=> setSearchItem(e.target.value)}
-        />
-      </div>
-      <Grid search={searchItem}></Grid>
-      {isOpen && (
-        <div className="modal-overlay">
-          <div className="modal-container">
-            Adicione sua resenha
-            <form action="POST" className="flex-column ">
-              <label>
-                Filme:
-                <input type="text" />
-              </label>
-              <label>
-                Sua Resenha:
-                <input type="text" />
-              </label>
-              <label>
-                Sua nota:
-                <input type="text" />
-              </label>
-
-              <div className="flex g-12">
-                <button
-                  onClick={closeModal}
-                  className="rounded-3 shadow-lighter"
-                >
-                  Fecha
-                </button>
-                <button
-                  onClick={closeModal}
-                  className="rounded-3 shadow-lighter"
-                >
-                  Confirma
-                </button>
-              </div>
-            </form>
-          </div>
+      <div className="g-24 component-div">
+        <div className="g-24 align-center">
+          <h1>Resenha de Filmes</h1>
+          <button onClick={openModal} className="rounded-3 shadow-lighter">
+            Adicionar resenha
+          </button>
+          <input
+            type="search"
+            name=""
+            id=""
+            placeholder="Procure um filme"
+            className="p-10 rounded-3 shadow-lighter"
+            value={searchItem}
+            onChange={(e)=> setSearchItem(e.target.value)}
+          />
         </div>
-      )}
-    </div>
+        <Grid search={searchItem}></Grid>
+        {isOpen && (
+          <div className="modal-overlay">
+            <div className="modal-container">
+              Adicione sua resenha
+              <form action="POST" className="flex-column ">
+                <label>
+                  Filme:
+                  <input type="text" />
+                </label>
+                <label>
+                  Sua Resenha:
+                  <input type="text" />
+                </label>
+                <label>
+                  Sua nota:
+                  <input type="text" />
+                </label>
+                <div className="flex g-12">
+                  <button
+                    onClick={closeModal}
+                    className="rounded-3 shadow-lighter"
+                  >
+                    Fecha
+                  </button>
+                  <button
+                    onClick={closeModal}
+                    className="rounded-3 shadow-lighter"
+                  >
+                    Confirma
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>
   );
 };
 
--- crud_resenha_filme/src/ui/components/grid.tsx
+++ crud_resenha_filme/src/ui/components/grid.tsx
@@ -1,37 +1,78 @@
+import { useState } from "react";
 import useGetReviews from "../../data/reviews.tsx";
 
-const Grid = ({search} : {search:string}) => {
+const Grid = ({ search }: { search: string }) => {
   const { data, error, loading } = useGetReviews();
-  
+  const [pageNumber, setPageNumber] = useState(1);
+  const headers = ["#", "Nome do Filme", "Resenha", "Nota", "Ações"];
+  const limit = 5;
+  const shownLength = Math.ceil(data?.length / limit);
+
+  const pagesList = [];
+  for (let index = 1; index <= shownLength; index++) {
+    pagesList.push(index);
+  }
+
+  const filteredData = data.filter((item) =>
+    item.nome.toLowerCase().includes(search.trim())
+  );
 
   return (
     <>
       <div className="grid-header">
-        <div>#</div>
-        <div>Nome do filme</div>
-        <div>Resenha</div>
-        <div>Nota</div>
-        <div>Ações</div>
+        {headers.map((item) => (
+          <div className="justify-center flex g-4">{item}</div>
+        ))}
       </div>
       <div>
-        {/* Colunas */}
-        {loading && <div className="justify-center align-center font-size-18">Carregando...</div>}
-        {error && <div className="justify-center align-center font-size-18">Poxa, tivemos um erro para buscar as resenhas.</div>}
+        {loading && (
+          <div className="justify-center align-center font-size-18">
+            Carregando...
+          </div>
+        )}
+        {error && (
+          <div className="justify-center align-center font-size-18">
+            Poxa, tivemos um erro para buscar as resenhas.
+          </div>
+        )}
         {data &&
-          data
-          .filter((item)=> item.nome.toLowerCase().includes(search))
-          .map((item, index) => (
-            <div className="grid">
-              <div>{index + 1}</div>
-              <div>{item.nome}</div>
-              <div>{item.resenha}</div>
-              <div>{item.nota}</div>
-              <div className="flex justify-center g-12 ">
-                <button className="rounded-3 shadow-lighter">Editar</button>
-                <button className="rounded-3 shadow-lighter">Deletar</button>
+          filteredData
+            .filter(
+              (item, index) =>
+                index + 1 <= pageNumber * limit &&
+                index + 1 >= pageNumber * limit - limit
+            )
+            .map((item, index) => (
+              <div className="grid">
+                <div>{item.id} </div>
+                <div>{item.nome}</div>
+                <div>{item.resenha}</div>
+                <div>{item.nota}</div>
+                <div className="flex justify-center g-12 ">
+                  <button>Editar</button>
+                  <button>Deletar</button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+        {data ? (
+                <div className="flex justify-between align-center">
+                  <div>
+                    Mostrando {limit} de {data.length} avaliações
+                  </div>
+                  <div className="justify-around g-6">
+                    {pagesList.map((item) => (
+                      <button
+                        onClick={(e) =>
+                          setPageNumber(Number.parseInt(e.currentTarget.value))
+                        }
+                        value={item}
+                      >
+                        {item}
+                      </button>
+                    ))}
+                  </div>
+                </div>
+        ) : null}
       </div>
     </>
   );
