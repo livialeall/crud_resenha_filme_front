ed594bd1e16728ad7af5d5b17bcccb5333c13429
Lívia Leal <liviabenine@gmail.com>
2024-12-17 20:09:16 -0300

    Modal para confirmar para deletar informação

--- crud_resenha_filme/src/ui/components/grid.tsx
+++ crud_resenha_filme/src/ui/components/grid.tsx
@@ -4,36 +4,49 @@ import Pagination from "./pagination.tsx";
 
 const Grid = ({ search }: { search: string }) => {
   const [pageNumber, setPageNumber] = useState(1);
-  const [deletedItems,setDeletedItems] = useState([])
+  const [deletedItems, setDeletedItems] = useState<string[]>([]);
+  const [itemToBeDeleted, setItemToBeDeleted] = useState<Review>();
   const headers = ["Nome do Filme", "Resenha", "Nota", "Ações"];
   const limit = 5;
   const { data, error, loading } = useGetReviews();
-
-  const cleanedData = data.filter((item)=> !deletedItems.includes(item.id.toString()))
+  const [modalConfirmDelete, setModalConfirmDelete] = useState(false);
+  const cleanedData = data.filter(
+    (item) => !deletedItems.includes(item.id.toString())
+  );
   console.log(cleanedData);
   const searchedData = cleanedData.filter((item) =>
     item.nome.toLowerCase().includes(search.toLowerCase().trim())
   );
 
-  const quantity = Math.ceil( searchedData.length == 0 ? data.length / limit : searchedData.length / limit);
+  const quantity = Math.ceil(
+    searchedData.length == 0 ? data.length / limit : searchedData.length / limit
+  );
 
-  useEffect(() =>{
-    setPageNumber(1)
-  },[search])
+  useEffect(() => {
+    setPageNumber(1);
+  }, [search]);
 
   const startIndex = (pageNumber - 1) * limit;
   const endIndex = startIndex + limit;
-  const paginatedData =  searchedData.slice(startIndex, endIndex);
+  const paginatedData = searchedData.slice(startIndex, endIndex);
 
   const pagesList = [];
   for (let index = 1; index <= quantity; index++) {
     pagesList.push(index);
   }
 
-  const handleDeleteButton = (id : number) => {
-    console.log(id)
-    setDeletedItems((prev) => [...prev,id])
-  }
+  const handleConfirmDeleteButton = (id:string) => {
+    console.log("Clicou para abrir")
+    console.log(id);
+    const teste = data.filter((item) => item.id.toString() == id);
+    setItemToBeDeleted(teste[0])
+    setModalConfirmDelete(true)
+  };
+
+  const handleDeleteButton = (id: string) => {
+    setDeletedItems((prev) => [...prev, id]);
+    setModalConfirmDelete(false)
+  };
 
   return (
     <>
@@ -63,14 +76,35 @@ const Grid = ({ search }: { search: string }) => {
               </div>
               <div className="flex justify-center g-12 ">
                 <button>Editar</button>
-                <button onClick={(e) => handleDeleteButton(e.currentTarget.value)} value={item.id}>Deletar</button>
+                <button value={item.id} onClick={(e) => handleConfirmDeleteButton(e.currentTarget.value)}>
+                  Deletar
+                </button>
               </div>
             </div>
           ))}
+          {modalConfirmDelete && (
+                <div className="modal-overlay">
+                  <div className="modal-container">
+                    <span>Tem certeza que quer deletar a resenha do filme: {itemToBeDeleted?.nome}</span>
+                    <div className="flex justify-center g-12 m-top-4">
+                      <button
+                        value={itemToBeDeleted?.id}
+                        onClick={(e) => handleDeleteButton(e.currentTarget.value)}
+                      >
+                        Confirmar
+                      </button>
+                      <button onClick={() => setModalConfirmDelete(false)}>
+                        Cancelar
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              )}
         {searchedData.length > 0 ? (
           <div className="flex justify-between align-center">
             <div>
-              Mostrando {paginatedData.length} de {searchedData.length} avaliações
+              Mostrando {paginatedData.length} de {searchedData.length}{" "}
+              avaliações
             </div>
             <Pagination
               pagesList={pagesList}
@@ -78,11 +112,11 @@ const Grid = ({ search }: { search: string }) => {
               onPageChange={setPageNumber}
             ></Pagination>
           </div>
-        ) : 
-        <div className="justify-center align-center font-size-18">
+        ) : (
+          <div className="justify-center align-center font-size-18">
             Não foram encontradas resenhas
           </div>
-        }
+        )}
       </div>
     </>
   );
